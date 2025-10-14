7f3982e5e604df846edbc29c0c877ef705c6a388
MTKimmins <mtkimmins@gmail.com>
2025-10-13 21:45:30 -0600

    Update main.py

--- PureBin/main.py
+++ PureBin/main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import ttk
 
 BIN0PATH = ""
 BIN1PATH = ""
@@ -25,23 +27,36 @@ reportCSVByte4 = None
 
 # OnStart announce what to do -> "OK"
 def open_run_window():
-    runWindow = tk.Toplevel(window)
+    runWindow = tk.Toplevel(root)
     runInstructions = tk.Label(runWindow)
     runInstructions.pack()
 
+# Select for upload
+def select_file(self):
+    fileTypes = [("Binary Files", "*.bin"), ("All Files", "*.*")]
+    filepaths = fd.askopenfilenames(
+        title="Select 5 Binary Dump Files...",
+        filetypes=fileTypes
+    )
+
+
 # Make a window
-window = tk.Tk()
-window.title = "Pure-BIN 5-to-1 Transformer"
+root = tk.Tk()
+root.title("Pure-BIN 5-to-1 Transformer")
+
+mainframe = ttk.Frame(root, padding=(3,3,12,12))
+mainframe.grid(column=0,row=0,sticky=("N","W","E","S"))
 
 # Make the buttons (Start, Exit)
-startButton = tk.Button(window, text="Start", command=open_run_window)
-startButton.pack()
-exitButton = tk.Button(window, text="Exit")
-exitButton.pack()
-window.mainloop()
+startButton = tk.Button(mainframe, text="Start", command=open_run_window)
+startButton.grid(column=0,row=0, sticky=("W","E"))
+exitButton = tk.Button(mainframe, text="Exit")
+exitButton.grid(column=0,row=1)
+root.bind("<Return>", open_run_window)
+root.mainloop()
 
 
-# Select for upload
+
 # Upload exactly 5 (can upload many, but will overwrite the current arrangement and populate from 0)
 # Display paths beside assigned name
 # Run
